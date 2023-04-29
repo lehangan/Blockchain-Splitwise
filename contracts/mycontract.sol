699a1ca44c933188f4b7146a197d6cd798c2d937
lehangan <lehangan30@gmail.com>
2023-04-29 18:07:01 +0700

    commit

--- contracts/mycontract.sol
+++ contracts/mycontract.sol
@@ -6,8 +6,50 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract Splitwise {
-// DO NOT MODIFY ABOVE THIS
+    mapping(address => mapping(address => uint32)) public debt;
+    address[] public users;
 
-// ADD YOUR CONTRACT CODE BELOW
+    function lookup(
+        address debtor,
+        address creditor
+    ) public view returns (uint32 ret) {
+        ret = debt[debtor][creditor];
+        return ret;
+    }
 
+    function add_IOU(address creditor, uint32 amount) public {
+        add_IOU_user(msg.sender, creditor, amount);
+    }
+
+    function add_IOU_user(
+        address debtor,
+        address creditor,
+        uint32 amount
+    ) public {
+        debt[debtor][creditor] += amount;
+        addUser(creditor);
+        addUser(debtor);
+    }
+
+    function add_IOU_sub(
+        address debtor,
+        address creditor,
+        uint32 amount
+    ) public {
+        debt[debtor][creditor] -=amount;
+    }
+
+    function addUser(address userAdd) internal {
+        for (uint i = 0; i < users.length; i++) {
+            if (users[i] == userAdd) return;
+        }
+        users.push(userAdd);
+    }
+
+    function getUsers() public view returns (address[] memory _users) {
+        _users = new address[](users.length);
+        for (uint i = 0; i < users.length; i++){
+            _users[i] = users[i];
+        }
+    }
 }
--- web_app/script.js
+++ web_app/script.js
@@ -9,12 +9,157 @@ var GENESIS = '0x000000000000000000000000000000000000000000000000000000000000000
 
 // This is the ABI for your contract (get it from Remix, in the 'Compile' tab)
 // ============================================================
-var abi = []; // FIXME: fill this in with your contract's ABI //Be sure to only have one array, not two
+var abi = [
+	{
+		"inputs": [
+		  {
+			"internalType": "address",
+			"name": "creditor",
+			"type": "address"
+		  },
+		  {
+			"internalType": "uint32",
+			"name": "amount",
+			"type": "uint32"
+		  }
+		],
+		"name": "add_IOU",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	  },
+	  {
+		"inputs": [
+		  {
+			"internalType": "address",
+			"name": "debtor",
+			"type": "address"
+		  },
+		  {
+			"internalType": "address",
+			"name": "creditor",
+			"type": "address"
+		  },
+		  {
+			"internalType": "uint32",
+			"name": "amount",
+			"type": "uint32"
+		  }
+		],
+		"name": "add_IOU_sub",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	  },
+	  {
+		"inputs": [
+		  {
+			"internalType": "address",
+			"name": "debtor",
+			"type": "address"
+		  },
+		  {
+			"internalType": "address",
+			"name": "creditor",
+			"type": "address"
+		  },
+		  {
+			"internalType": "uint32",
+			"name": "amount",
+			"type": "uint32"
+		  }
+		],
+		"name": "add_IOU_user",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	  },
+	  {
+		"inputs": [
+		  {
+			"internalType": "address",
+			"name": "",
+			"type": "address"
+		  },
+		  {
+			"internalType": "address",
+			"name": "",
+			"type": "address"
+		  }
+		],
+		"name": "debt",
+		"outputs": [
+		  {
+			"internalType": "uint32",
+			"name": "",
+			"type": "uint32"
+		  }
+		],
+		"stateMutability": "view",
+		"type": "function"
+	  },
+	  {
+		"inputs": [],
+		"name": "getUsers",
+		"outputs": [
+		  {
+			"internalType": "address[]",
+			"name": "_users",
+			"type": "address[]"
+		  }
+		],
+		"stateMutability": "view",
+		"type": "function"
+	  },
+	  {
+		"inputs": [
+		  {
+			"internalType": "address",
+			"name": "debtor",
+			"type": "address"
+		  },
+		  {
+			"internalType": "address",
+			"name": "creditor",
+			"type": "address"
+		  }
+		],
+		"name": "lookup",
+		"outputs": [
+		  {
+			"internalType": "uint32",
+			"name": "ret",
+			"type": "uint32"
+		  }
+		],
+		"stateMutability": "view",
+		"type": "function"
+	  },
+	  {
+		"inputs": [
+		  {
+			"internalType": "uint256",
+			"name": "",
+			"type": "uint256"
+		  }
+		],
+		"name": "users",
+		"outputs": [
+		  {
+			"internalType": "address",
+			"name": "",
+			"type": "address"
+		  }
+		],
+		"stateMutability": "view",
+		"type": "function"
+	  }
+]; // FIXME: fill this in with your contract's ABI //Be sure to only have one array, not two
 // ============================================================
 abiDecoder.addABI(abi);
 // call abiDecoder.decodeMethod to use this - see 'getAllFunctionCalls' for more
 
-var contractAddress = ""; // FIXME: fill this in with your contract's address/hash
+var contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // FIXME: fill this in with your contract's address/hash
 
 var BlockchainSplitwise = new ethers.Contract(contractAddress, abi, provider.getSigner());
 
@@ -27,26 +172,86 @@ var BlockchainSplitwise = new ethers.Contract(contractAddress, abi, provider.get
 // TODO: Return a list of all users (creditors or debtors) in the system
 // All users in the system are everyone who has ever sent or received an IOU
 async function getUsers() {
-
+	return await BlockchainSplitwise.getUsers();
 }
 
+async function lookup(debtor, creditor) {
+	return await BlockchainSplitwise.lookup(debtor, creditor);
+}
 // TODO: Get the total amount owed by the user specified by 'user'
 async function getTotalOwed(user) {
+	var total = 0;
+	var users = await getUsers();
 
+	for (var i = 0; i < users.length; i++) {
+		total += await lookup(user, users[i]);
+		//console.log(total);
+	}
+	return total;
 }
 
 // TODO: Get the last time this user has sent or received an IOU, in seconds since Jan. 1, 1970
 // Return null if you can't find any activity for the user.
 // HINT: Try looking at the way 'getAllFunctionCalls' is written. You can modify it if you'd like.
 async function getLastActive(user) {
-	
+	var functioncall = getAllFunctionCalls(contractAddress, 'add_IOU');
+	var time = 0;
+	var num = (await functioncall).length;
+	var activity;
+	for (var i = 0; i < num; i++) {
+		if ((await functioncall)[i].from == user.toLowerCase() || (await functioncall)[i].args[0] == user.toLowerCase()) {
+			activity = (await functioncall)[i].t;
+			break;
+		}
+		//return (await functioncall)[i].t;
+	}
+	return activity;
+
 }
 
+async function getNeighbors(user) {
+	var neighbors = [];
+	var users = await getUsers();
+	for (var i = 0; i < users.length; i++) {
+		var owed = await lookup(user, users[i]);
+		if (owed > 0) neighbors.push(users[i]);
+	}
+	return neighbors;
+}
 // TODO: add an IOU ('I owe you') to the system
 // The person you owe money is passed as 'creditor'
 // The amount you owe them is passed as 'amount'
-async function add_IOU(creditor, amount) {
+
+async function add_IOU(creditor, amount, account) {
+	//creditor is owned , account is debtor 
+	var users = await getUsers();
+	var start = users[0];
+	const signer = await provider.getSigner(account);
+	await BlockchainSplitwise.connect(signer).add_IOU(creditor, amount);
+
+	var path = await doBFS(creditor, account, getNeighbors);
+	//alert(path);
+	var min=0;
+	if (path != null) {
+		var number = await path.length;
+		min = amount;
+		for (var i = 0; i < number - 1; i++) {
+			var amount2 = await lookup(path[i], path[i + 1]);
+			if (amount2 < min) {
+				min = amount2;
+			}
+		}
+		for( var i = 0; i<number - 1 ; i++ )
+			{
+				 await BlockchainSplitwise.add_IOU_sub(path[i], path[i+1], min);
+			}
+		await BlockchainSplitwise.add_IOU_sub(account, creditor, min);
+	}
 	
+	// const signer = await provider.getSigner(account);
+	// //alert(signer);
+	// await BlockchainSplitwise.connect(signer).add_IOU(creditor, amount);
+
 }
 
 // =============================================================================
@@ -61,29 +266,29 @@ async function getAllFunctionCalls(addressOfContract, functionName) {
 	var function_calls = [];
 
 	while (curBlock !== GENESIS) {
-	  var b = await provider.getBlockWithTransactions(curBlock);
-	  var txns = b.transactions;
-	  for (var j = 0; j < txns.length; j++) {
-	  	var txn = txns[j];
+		var b = await provider.getBlockWithTransactions(curBlock);
+		var txns = b.transactions;
+		for (var j = 0; j < txns.length; j++) {
+			var txn = txns[j];
 
-	  	// check that destination of txn is our contract
-			if(txn.to == null){continue;}
-	  	if (txn.to.toLowerCase() === addressOfContract.toLowerCase()) {
-	  		var func_call = abiDecoder.decodeMethod(txn.data);
+			// check that destination of txn is our contract
+			if (txn.to == null) { continue; }
+			if (txn.to.toLowerCase() === addressOfContract.toLowerCase()) {
+				var func_call = abiDecoder.decodeMethod(txn.data);
 
 				// check that the function getting called in this txn is 'functionName'
 				if (func_call && func_call.name === functionName) {
 					var timeBlock = await provider.getBlock(curBlock);
-		  		var args = func_call.params.map(function (x) {return x.value});
-	  			function_calls.push({
-	  				from: txn.from.toLowerCase(),
-	  				args: args,
+					var args = func_call.params.map(function (x) { return x.value });
+					function_calls.push({
+						from: txn.from.toLowerCase(),
+						args: args,
 						t: timeBlock.timestamp
-	  			})
-	  		}
-	  	}
-	  }
-	  curBlock = b.parentHash;
+					})
+				}
+			}
+		}
+		curBlock = b.parentHash;
 	}
 	return function_calls;
 }
@@ -95,13 +300,16 @@ async function doBFS(start, end, getNeighbors) {
 	var queue = [[start]];
 	while (queue.length > 0) {
 		var cur = queue.shift();
-		var lastNode = cur[cur.length-1]
+		// shift() removes the first item of an array
+		// or shift() returns the removed array element ( first element )
+		var lastNode = cur[cur.length - 1]
 		if (lastNode.toLowerCase() === end.toString().toLowerCase()) {
 			return cur;
 		} else {
 			var neighbors = await getNeighbors(lastNode);
 			for (var i = 0; i < neighbors.length; i++) {
 				queue.push(cur.concat([neighbors[i]]));
+				// concat join the array 
 			}
 		}
 	}
@@ -114,51 +322,54 @@ async function doBFS(start, end, getNeighbors) {
 
 // This sets the default account on load and displays the total owed to that
 // account.
-provider.listAccounts().then((response)=> {
+provider.listAccounts().then((response) => {
 	defaultAccount = response[0];
 
-	getTotalOwed(defaultAccount).then((response)=>{
-		$("#total_owed").html("$"+response);
+	getTotalOwed(defaultAccount).then((response) => {
+		$("#total_owed").html("$" + response);
 	});
 
-	getLastActive(defaultAccount).then((response)=>{
+	getLastActive(defaultAccount).then((response) => {
 		time = timeConverter(response)
 		$("#last_active").html(time)
 	});
 });
 
 // This code updates the 'My Account' UI with the results of your functions
-$("#myaccount").change(function() {
+$("#myaccount").change(function () {
 	defaultAccount = $(this).val();
 
-	getTotalOwed(defaultAccount).then((response)=>{
-		$("#total_owed").html("$"+response);
+	getTotalOwed(defaultAccount).then((response) => {
+		$("#total_owed").html("$" + response);
 	})
 
-	getLastActive(defaultAccount).then((response)=>{
-		time = timeConverter(response)
-		$("#last_active").html(time)
+	getLastActive(defaultAccount).then((response) => {
+		time = timeConverter(response);
+		$("#last_active").html(time);
 	});
 });
 
 // Allows switching between accounts in 'My Account' and the 'fast-copy' in 'Address of person you owe
-provider.listAccounts().then((response)=>{
-	var opts = response.map(function (a) { return '<option value="'+
-			a.toLowerCase()+'">'+a.toLowerCase()+'</option>' });
+provider.listAccounts().then((response) => {
+	var opts = response.map(function (a) {
+		return '<option value="' +
+			a.toLowerCase() + '">' + a.toLowerCase() + '</option>'
+	});
 	$(".account").html(opts);
-	$(".wallet_addresses").html(response.map(function (a) { return '<li>'+a.toLowerCase()+'</li>' }));
+	$(".wallet_addresses").html(response.map(function (a) { return '<li>' + a.toLowerCase() + '</li>' }));
 });
 
 // This code updates the 'Users' list in the UI with the results of your function
-getUsers().then((response)=>{
-	$("#all_users").html(response.map(function (u,i) { return "<li>"+u+"</li>" }));
+getUsers().then((response) => {
+	$("#all_users").html(response.map(function (u, i) { return "<li>" + u + "</li>" }));
 });
 
 // This runs the 'add_IOU' function when you click the button
 // It passes the values from the two inputs above
-$("#addiou").click(function() {
+$("#addiou").click(function () {
 	defaultAccount = $("#myaccount").val(); //sets the default account
-  add_IOU($("#creditor").val(), $("#amount").val()).then((response)=>{
+	// alert(defaultAccount);
+	add_IOU($("#creditor").val(), $("#amount").val(), defaultAccount).then((response) => {
 		window.location.reload(false); // refreshes the page after add_IOU returns and the promise is unwrapped
 	})
 });
@@ -192,40 +403,53 @@ function check(name, condition) {
 }
 
 async function sanityCheck() {
-	console.log ("\nTEST", "Simplest possible test: only runs one add_IOU; uses all client functions: lookup, getTotalOwed, getUsers, getLastActive");
+	// console.log ("\nTEST", "Simplest possible test: only runs one add_IOU; uses all client functions: lookup, getTotalOwed, getUsers, getLastActive");
 
-	var score = 0;
+	// var score = 0;
+
+	// var accounts = await provider.listAccounts();
+	// defaultAccount = accounts[0];
+
+	// var users = await getUsers();
+	// score += check("getUsers() initially empty", users.length === 0);
+
+	// var owed = await getTotalOwed(accounts[1]);
+	// score += check("getTotalOwed(0) initially empty", owed === 0);
+
+	// var lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
+	// console.log("lookup(0, 1) current value" + lookup_0_1);
+	// score += check("lookup(0,1) initially 0", parseInt(lookup_0_1, 10) === 0);
+
+	// var response = await add_IOU(accounts[1], "10");
+
+	// users = await getUsers();
+	// score += check("getUsers() now length 2", users.length === 2);
+
+	// owed = await getTotalOwed(accounts[0]);
+	// score += check("getTotalOwed(0) now 10", owed === 10);
+
+	// lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
+	// score += check("lookup(0,1) now 10", parseInt(lookup_0_1, 10) === 10);
+
+	// var timeLastActive = await getLastActive(accounts[0]);
+	// var timeNow = Date.now()/1000;
+
+	// var difference = timeNow - timeLastActive;
+	// score += check("getLastActive(0) works", difference <= 60 && difference >= -3); // -3 to 60 seconds
+
+	// console.log("Final Score: " + score +"/21");
 
 	var accounts = await provider.listAccounts();
-	defaultAccount = accounts[0];
-
 	var users = await getUsers();
-	score += check("getUsers() initially empty", users.length === 0);
+	console.log("users length ", users);
+	var owed = await getTotalOwed(accounts[0]);
+	console.log(" total owed", owed);
+	var last_activity = await getLastActive(accounts[0]);
+	console.log("last activity ", last_activity);
+	var neighborshood = await getNeighbors(accounts[0]);
+	console.log("neighborhood ", neighborshood);
+	//var path = await add_IOU(accounts[0],accounts[0],)
 
-	var owed = await getTotalOwed(accounts[1]);
-	score += check("getTotalOwed(0) initially empty", owed === 0);
-
-	var lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
-	console.log("lookup(0, 1) current value" + lookup_0_1);
-	score += check("lookup(0,1) initially 0", parseInt(lookup_0_1, 10) === 0);
-
-	var response = await add_IOU(accounts[1], "10");
-
-	users = await getUsers();
-	score += check("getUsers() now length 2", users.length === 2);
-
-	owed = await getTotalOwed(accounts[0]);
-	score += check("getTotalOwed(0) now 10", owed === 10);
-
-	lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
-	score += check("lookup(0,1) now 10", parseInt(lookup_0_1, 10) === 10);
-
-	var timeLastActive = await getLastActive(accounts[0]);
-	var timeNow = Date.now()/1000;
-	var difference = timeNow - timeLastActive;
-	score += check("getLastActive(0) works", difference <= 60 && difference >= -3); // -3 to 60 seconds
-
-	console.log("Final Score: " + score +"/21");
 }
 
-// sanityCheck() //Uncomment this line to run the sanity check when you first open index.html
+sanityCheck() //Uncomment this line to run the sanity check when you first open index.html
